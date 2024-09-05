3f9295690fa7a2f970a8e2f5ee2834691b687452
Jonas Wagner <jonas.wagner.2826@gmail.com>
2024-09-05 04:47:33 -0500

    updated reachability example for direct paper usage

--- examples/memZono/exampleMemoryReachability_memZono.m
+++ examples/memZono/exampleMemoryReachability_memZono.m
@@ -10,107 +10,94 @@ A = [   1, 0.25;
      -0.5, 0.75];
 B = [   0.025;
         0.25];
-
 n = size(A,1); m = size(B,2);
 
 %% Reachability Setup
-X_0 = zono(diag([1,2]),zeros(2,1));
-X_F = zono(0.25*eye(2),ones(2.,1));
-U_nom = zono(1.5,0);
-
-%% Reachability Calculation
 % Setup Labels
-varDims = @(var,n,k) memZono.genKeys(sprintf('%s_k=%d',var,k),1:n);
-xDims = @(k) varDims('x',n,k);
-if m > 1, uDims = @(k) varDims('u',m,k); 
-else, uDims = @(k) {sprintf('u_k=%d',k)}; end
+xDims = @(k) memZono.genKeys(sprintf('%s_k=%d',var,k),1:n);
+uDims = @(k) {sprintf('u_k=%d',k)}; 
 
 % Initial Conditions
+X_0 = zono(diag([1,2]),zeros(2,1));
 X_{1} = memZono(X_0,xDims(1));
 X_all = X_{1};
 
 % Terminal Set
+X_F = zono(0.25*eye(2),ones(2.,1));
 X_F = memZono(X_F,xDims(N));
 
+% Nominal Input Set
+U_nom = zono(1.5,0);
+
+%% Reachability Calculation
 switch 'method4' %'method1' 'method2' 'method3' 'method4'
-    case 'method1' % Calculate and Save
-        % Time-evolution
-        for k = 1:N-1
-            % Current Input
-            U_{k} = memZono(U_nom,uDims(k));
-
-            % Step Update
-            X_{k+1} = X_{k}.map(A,xDims(k),xDims(k+1)) ...
-                    + U_{k}.map(B,uDims(k),xDims(k+1));
-
-            % Save Data
-            % X_all = [X_all; U_{k}; X_{k+1}]; %<---- vertcat() = cartprod()
-            % X_all = vertcat(X_all, U_{k}, X_{k}); %<---- vertcat() = cartprod()
-            X_all = [
-                X_all; 
-                U_{k}; 
-                X_{k+1}
-            ]; %<---- vertcat() = cartprod()
-            % ^ @jruths... which to include???
-        end
-        X_inter = X_all.and(X_F,'terminal_cons');
+    case 'method1' 
+% Calculate and Save
+for k = 1:N-1 % Time-evolution
+    % Current Input
+    U_{k} = memZono(U_nom,uDims(k));
+    % Step Update
+    X_{k+1} = X_{k}.map(A,xDims(k),xDims(k+1)) ...
+            + U_{k}.map(B,uDims(k),xDims(k+1));
+    % Save Data ($\starcross$)
+    X_all = [X_all; U_{k}; X_{k+1}]; 
+end
+% Add Terminal Constraints
+X_inter = X_all.and(X_F, 'terminal_cons');
 
     case 'method2' % recursively calcualte and projection
-        % Use map(), plus(), cartProd(), and()
-        % Time-Evolution
-        for k = 1:N-1
-            % Current Input
-            U_k = memZono(U_nom,uDims(k));
-            X_all = cartProd(X_all, U_k);
+% map(), plus(), cartProd(), and()
+for k = 1:N-1 % Time-Evolution
+% Current Input
+U_k = memZono(U_nom,uDims(k));
+X_all = cartProd(X_all, U_k);
+% Step Update
+X_all = cartProd(X_all,...
+    plus(X_all.map(A,xDims(k),xDims(k+1)),...
+        X_all.map(B,uDims(k),xDims(k+1))));
+end
+% Add Terminal Constraints
+X_inter = and(X_all,X_F, 'terminal_cons');
+% Time-projections
+for k = 1:N-1
+    X_{k} = X_all(xDims(k));
+    U_{k} = X_all(uDims(k));
+end
+X_{N} = X_all(xDims(N));
 
-            % Time-Update: X_{k+1} = A X_{k} \oplus B U_{k} 
-            X_all = cartProd(X_all,...
-                plus(map(X_all,A,xDims(k),xDims(k+1)),...
-                    map(X_all,B,uDims(k),xDims(k+1))));
-        end
-        % Add Terminal Constraints
-        X_inter = and(X_all,X_F,'terminal_cons');
-
-        % time-projections
-        for k=1:N; X_{k} = X_all(xDims(k)); end
-        for k=1:N-1; U_{k} = X_all(uDims(k)); end
+for k=1:N; X_{k}=X_all(xDims(k)); end
+for k=1:N-1;U_{k}=X_all(uDims(k));end
 
             
     case 'method3' % map(), plus(), cartProd(), and()
-        % time-Evolution
-        for k = 1:N-1
-            % Current Input
-            X_all = cartProd(X_all,memZono(U_nom,uDims(k)));
-
-            % time-setp update
-            X_all = cartProd(X_all,...
-                plus(map(X_all,A,xDims(k),xDims(k+1)),...
-                    map(X_all,B,uDims(k),xDims(k+1))));
-        end
-        % add Terminal Constraints
-        X_inter = and(X_all,X_F,'terminal_cons');
-
-        % time-projections
-        X_ = arrayfun(@(k) X_all(xDims(k)),1:N,UniformOutput=false);
-        U_ = arrayfun(@(k) X_all(uDims(k)),1:N-1,UniformOutput=false);
-        % for k=1:N, X_{k} = projection(X_all,xDims(k)); end
-        % for k=1:N-1, U_{k} = projection(X_all,uDims(k)); end
-        %^ @jruths... which to include???????
+% time-Evolution
+for k = 1:N-1
+    % Current Input
+    X_all = cartProd(X_all,memZono(U_nom,uDims(k)));
+    % Recursive Set Update
+    X_all = cartProd(X_all,...
+        plus(map(X_all,A,xDims(k),xDims(k+1)),...
+            map(X_all,B,uDims(k),xDims(k+1))));
+end
+% Add Terminal Constraints
+X_inter = and(X_all,X_F,'terminal_cons');
+% Set Projections
+X_ = arrayfun(@(k) X_all(xDims(k)),1:N,UniformOutput=false);
+U_ = arrayfun(@(k) X_all(uDims(k)),1:N-1,UniformOutput=false);
 
     case 'method4' % Recursive Map and Save
-        % Time-Evolution
-        for k = 1:N-1
-            % Current Input
-            U_{k} = memZono(U_nom,uDims(k));
-            X_all = [X_all; U_{k}];
-
-            % Time-Update
-            X_{k+1} = X_all.map(A,xDims(k),xDims(k+1)) ...
-                    + X_all.map(B,uDims(k),xDims(k+1));
-            X_all = [X_all; X_{k+1}];
-        end
-        % Add Terminal Constraints
-        X_inter = and(X_all,X_F,'terminal_cons');
+% Time-Evolution
+for k = 1:N-1
+    % Current Input
+    U_{k} = memZono(U_nom,uDims(k));
+    X_all = [X_all; U_{k}];
+    % Time-Update
+    X_{k+1} = X_all.map(A,xDims(k),xDims(k+1)) ...
+            + X_all.map(B,uDims(k),xDims(k+1));
+    X_all = [X_all; X_{k+1}];
+end
+% Add Terminal Constraints
+X_inter = and(X_all,X_F,'terminal_cons');
 end
 
 %% Plotting
@@ -154,3 +141,11 @@ function color = selectColor(i)
     colors = {'k','b','r'};
     color = colors{mod(i,length(colors))+1};
 end
+
+
+%% Old Code
+% varDims = @(var,n,k) memZono.genKeys(sprintf('%s_k=%d',var,k),1:n);
+% xDims = @(k) varDims('x',n,k);
+% if m > 1, uDims = @(k) varDims('u',m,k); 
+% else, uDims = @(k) {sprintf('u_k=%d',k)}; end
+
